d528af3743a63417c49352dd587b75966caed8d4
mikesealey <mikesealey@hotmail.com>
2024-09-05 17:53:44 +0100

    adds sizing options for barcode and QR code

--- src/Component.svelte
+++ src/Component.svelte
@@ -5,8 +5,12 @@
 	import { createQrSvgString, createQrSvgDataUrl } from '@svelte-put/qr';
   
   export let value;
-  export let showQR = true;
+  export let showQR;
   export let showValue;
+  export let customLogo;
+  export let size;
+  export let primColor;
+  export let secColor;
 
   const { styleable } = getContext("sdk");
   const component = getContext("component");
@@ -16,9 +20,13 @@
   let qrContainer;
 
   const generateBarcode = () => {
+    let barcodeSize = size / 100
     if (value) {
       JsBarcode("#barcode", value, {
         displayValue: showValue,
+        width: barcodeSize,
+        lineColor: primColor,
+        background: secColor
       });
     }
   };
@@ -45,13 +53,25 @@ console.log("generating QR code")
 </script>
 
 <div class="overall" use:styleable={$component.styles}>
+  <p>{primColor} {secColor}</p>
   {#if value}
     {#if showQR}
     <div>
-      {@html createQrSvgString({data: value, moduleFill: "black", anchorOuterFill: "black", anchorInnerFill: "black", width: 200, height: 200})}
+      {@html createQrSvgString({
+        data: value, logo: customLogo, 
+        moduleFill: primColor, 
+        anchorOuterFill: primColor, 
+        anchorInnerFill: primColor, 
+        backgroundFill: secColor, 
+        width: size, 
+        height: size
+        })}
+      <div style="word-wrap: break-word; overflow-wrap: break-word; width: {size};">{showValue ? value : ""}</div>
     </div>
     {:else}
+    <div >
       <img id="barcode" alt="barcode {value ? value : ""}"/>
+    </div>
     {/if}
   {:else}
     <p>Please add a value to generate your {showQR ? "QR Code" : "Barcode"}</p>
